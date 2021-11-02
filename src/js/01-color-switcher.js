fba578c47ef733e3ae9493826774a9fb129e4d54
KaterinaBondarenko <kat9bondarenko@gmail.com>
2021-11-02 15:12:51 +0200

    add 01-colors-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+// <button type="button" data-start>Start</button>
+// <button type="button" data-stop>Stop</button>
+
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const bodyElem = document.querySelector('body');
+
+startBtn.addEventListener('click', changeColor);
+
+let timerId = null;
+
+function changeColor() {
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+  timerId = setInterval(() => {
+    const color = getRandomHexColor();
+    bodyElem.style.backgroundColor = color;
+  }, 1000);
+}
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(timerId);
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+});
